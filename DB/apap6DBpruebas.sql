413b4855f57224fcbcbe7a1b849f8855eee99670
Veppam <v23e3c02@comunidad.unam.mx>
2020-04-14 15:17:53 -0500

    Clase Database

---
+++ DB/apap6DBpruebas.sql
@@ -0,0 +1,165 @@
+-- MariaDB dump 10.17  Distrib 10.4.11-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: apap6
+-- ------------------------------------------------------
+-- Server version	10.4.11-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `apap6`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `apap6` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `apap6`;
+
+--
+-- Table structure for table `material`
+--
+
+DROP TABLE IF EXISTS `material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `material` (
+  `id_material` int(3) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `cantidad` int(3) NOT NULL,
+  `dir_imagen` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_material`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `material`
+--
+
+LOCK TABLES `material` WRITE;
+/*!40000 ALTER TABLE `material` DISABLE KEYS */;
+INSERT INTO `material` VALUES (0,'Proyectores',5,'./imgs/proy.png'),(102,'Cables VGA',10,'./imgs/cVGA.png'),(124,'Adaptadores de audio 3.5mm a dos canales',15,'./imgs/adaptAudio35'),(342,'Bocinas',4,'./imgs/bocinas.png'),(548,'Control remoto de proyector',5,'./imgs/controlProy.png'),(629,'Cables de audio 3.5mm',15,'./imgs/cAudio35'),(731,'Cables HDMI',5,'./imgs/cHDMI'),(749,'Laptops',7,'./imgs/laptop.png');
+/*!40000 ALTER TABLE `material` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `material_prestado`
+--
+
+DROP TABLE IF EXISTS `material_prestado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `material_prestado` (
+  `id_material` int(3) NOT NULL,
+  `id_prestamo` int(2) NOT NULL,
+  `cantidad` int(3) NOT NULL,
+  KEY `id_material` (`id_material`),
+  KEY `id_prestamo` (`id_prestamo`),
+  CONSTRAINT `material_prestado_ibfk_1` FOREIGN KEY (`id_material`) REFERENCES `material` (`id_material`),
+  CONSTRAINT `material_prestado_ibfk_2` FOREIGN KEY (`id_prestamo`) REFERENCES `prestamo` (`id_prestamo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `material_prestado`
+--
+
+LOCK TABLES `material_prestado` WRITE;
+/*!40000 ALTER TABLE `material_prestado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `material_prestado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prestamo`
+--
+
+DROP TABLE IF EXISTS `prestamo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `prestamo` (
+  `id_prestamo` int(2) NOT NULL AUTO_INCREMENT,
+  `id_profesor` int(9) NOT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id_prestamo`),
+  KEY `id_profesor` (`id_profesor`),
+  CONSTRAINT `prestamo_ibfk_1` FOREIGN KEY (`id_profesor`) REFERENCES `profesor` (`id_profesor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prestamo`
+--
+
+LOCK TABLES `prestamo` WRITE;
+/*!40000 ALTER TABLE `prestamo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prestamo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profesor`
+--
+
+DROP TABLE IF EXISTS `profesor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profesor` (
+  `id_profesor` int(9) NOT NULL,
+  `ap_paterno` varchar(50) NOT NULL,
+  `ap_materno` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_profesor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profesor`
+--
+
+LOCK TABLES `profesor` WRITE;
+/*!40000 ALTER TABLE `profesor` DISABLE KEYS */;
+INSERT INTO `profesor` VALUES (123456785,'MATA','ACOLOSIO','SALINAS'),(123456786,'BRIEL','PORTILLO','JUANGA'),(123456787,'MORALES','RODRIGUEZ','CHENTE'),(123456788,'LOPEZ','RIVERA','LUPILLO'),(123456789,'MoLINILLO','TRASBESTI','JAIMITO');
+/*!40000 ALTER TABLE `profesor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `id_usuario` int(1) NOT NULL AUTO_INCREMENT,
+  `nombre_usuario` varchar(15) NOT NULL,
+  `contrasenia` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Elmer','Homero'),(2,'Susana','Distancia');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-14 12:22:11
---
+++ src/Database.java
@@ -0,0 +1,108 @@
+import java.sql.*;
+import java.util.Vector;
+
+public class Database {
+    private String URL= "jdbc:" +
+                        "mariadb://" +
+                        "localhost:" + //Dirección IP
+                        "3306/" + //Puerto
+                        "apap6"; //Nombre base de datos
+    private String USER="root";
+    private String PASSWORD="";
+
+    private Connection con;
+    private Statement sent;
+
+    public Database(){
+        try {
+            this.con= DriverManager.getConnection(URL,USER,PASSWORD);
+            this.sent= con.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ResultSet makeSqlCons(String sql){
+        ResultSet respuesta = null;
+        try {
+            respuesta = sent.executeQuery(sql);
+        } catch (SQLException e) {
+            System.out.println("Error en consulta SQL");
+        }
+        return respuesta;
+    }
+    public String[][] getDBUsers(){
+        String[] user1 = new String[2];
+        String[] user2 = new String[2];
+
+        String sql = "SELECT nombre_usuario, contrasenia FROM usuario";
+        ResultSet res = makeSqlCons(sql);
+        try {
+            if(res.next()){
+                user1[0]= res.getNString("nombre_usuario");
+                user1[1]= res.getNString("contrasenia");
+            }
+            if(res.next()){
+                user2[0]= res.getNString("nombre_usuario");
+                user2[1]= res.getNString("contrasenia");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error en resultados");
+        }
+
+        return new String[][]{user1, user2};
+    }
+
+    public Vector getDBMaterials(){
+        Vector materials= new Vector();
+        String sql= "SELECT * FROM material";
+        ResultSet res= makeSqlCons(sql);
+        boolean next=true;
+        while (next){
+            try {
+                if(res.next()) {
+                    Vector nMat = new Vector();
+                    nMat.addElement(res.getNString("id_material"));
+                    nMat.addElement(res.getNString("nombre"));
+                    nMat.addElement(res.getNString("cantidad"));
+                    nMat.addElement(res.getNString("dir_imagen"));
+                    materials.addElement(nMat);
+                }else
+                    next=false;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return materials;
+    }
+
+    public boolean matchTrabNum(String TN){
+        boolean exist= false;
+        String sql= "SELECT nombre FROM profesor WHERE id_profesor="+TN;
+        ResultSet res= makeSqlCons(sql);
+        try {
+            if(res.first())
+                exist = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return exist;
+    }
+
+    public String[] getDBprofessorData(String NT){
+        String[] nombre= new String[3];
+
+        String sql= "SELECT ap_paterno, ap_materno, nombre FROM profesor WHERE id_profesor= "+NT;
+        ResultSet res= makeSqlCons(sql);
+
+        try {
+            res.first();
+            nombre[0]= res.getNString("nombre");
+            nombre[1]= res.getNString("ap_materno");
+            nombre[2]= res.getNString("ap_paterno");
+        } catch (SQLException e) {
+            System.out.println("Error en consulta de nombre de profesor");
+        }
+        return nombre;
+    }
+}
