adb55ee8c47c5ece5d480a4fb2a641cef2785d8c
AntChong1226 <antoniochong1820@comunidad.unam.mx>
2020-04-17 10:46:32 -0500

    fix: Comentada la clase profesor

--- src/Profesor.java
+++ src/Profesor.java
@@ -1,10 +1,12 @@
 public class Profesor {
 
+    // Atributos de la calse Profesor
     private int numTrabajador;
     private String nombre;
     private String apPaterno;
     private String apMaterno;
 
+    // Constructor de la clase Profesor
     public Profesor ( int numTrab, String nom, String apPat, String apMat ) {
         numTrabajador = numTrab;
         nombre = nom;
@@ -12,18 +14,22 @@ public class Profesor {
         apMaterno = apMat;
     }
 
+    // Regresa el número de trabajador del profesor
     public int getNumTrabajador() {
         return numTrabajador;
     }
 
+    // Regresa el nombre del profesor
     public String getNombre() {
         return nombre;
     }
 
+    // Regresa el apellido paterno del profesor
     public String getApPaterno() {
         return apPaterno;
     }
 
+    // Regresa el apellido materno del profesor
     public String getApMaterno() {
         return apMaterno;
     }
